c78b799d013654b8c324f4016e607512b6f65512
youssouf994 <toure.youssouf.alassane@gmail.com>
2023-08-04 23:06:17 +0200

    registrazione e login completi 100% solo back

--- afro/app.py
+++ afro/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, g
 import bcrypt
 import sqlite3
 import secrets
@@ -42,41 +42,75 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/registrazione') 
-def crea_utente():
-    if request=='post':
-        id=request.form['#']
-        nome=request.form['#']
-        cognome=request.form['#']
-        tel=request.form['#']
-        citta=request.form['#']
-        mail=request.form['#']
-        passw=request.form['#']
+@app.route('/reg')
+def pag_registrazione():
+    return render_template('registrazione.html')
 
-        cursore=db_utenti().cursor()
+@app.route('/no', methods=['POST', 'GET'])
+def no():
+    return render_template('no.html')
+
+
+@app.route('/registrazione', methods=['POST', 'GET']) 
+def crea_utente():
+    if request.method=='POST':
+        nome=request.form['nome']
+        cognome=request.form['cognome']
+        tel=request.form['tel']
+        citta=request.form['citt']
+        mail=request.form['mail']
+        passw=request.form['pass']
+
+        data=db_utenti()
+        cursore=data.cursor()
+
 
         cursore.execute('SELECT * FROM utenti WHERE cell=?', (tel,))
         if cursore.fetchone() is not None:
-            return redirect(url_for('#'))
+            return render_template('no.html')
         else:
             #-----------CODIFICA VALORI INSERITI--------------
-            pass_codificata=bcrypt.haspw(passw.encode('utf-8'), bcrypt.gensalt())
-            mail_codificata=bcrypt.haspw(mail.encode('utf-8'), bcrypt.gensalt())
-            tel_codificata=bcrypt.haspw(tel.encode('utf-8'), bcrypt.gensalt())
-            city_codificata=bcrypt.haspw(citta.encode('utf-8'), bcrypt.gensalt())
-            cognome_codificata=bcrypt.haspw(cognome.encode('utf-8'), bcrypt.gensalt())
-            nome_codificata=bcrypt.haspw(nome.encode('utf-8'), bcrypt.gensalt())
-            id_codificata=bcrypt.haspw(id.encode('utf-8'), bcrypt.gensalt())
+            pass_codificata=bcrypt.hashpw(passw.encode('utf-8'), bcrypt.gensalt())
+            #mail_codificata=bcrypt.hashpw(mail.encode('utf-8'), bcrypt.gensalt())
+            #tel_codificata=bcrypt.hashpw(tel.encode('utf-8'), bcrypt.gensalt())
+            #city_codificata=bcrypt.hashpw(citta.encode('utf-8'), bcrypt.gensalt())
+            #cognome_codificata=bcrypt.hashpw(cognome.encode('utf-8'), bcrypt.gensalt())
+            #nome_codificata=bcrypt.hashpw(nome.encode('utf-8'), bcrypt.gensalt())
             #-----------------------------------------------------------
 
-            cursore.execute('INSERT INTO utenti(id_utenti, nome, cognome, mail, citta, cell, passwo) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                            (id, nome, cognome, tel, mail, citta, passw ))
+            cursore.execute('INSERT INTO utenti (nome, cognome, mail, citta, cell, passwo) VALUES (?, ?, ?, ?, ?, ?)',
+                            (nome, cognome, mail, citta, tel, pass_codificata ))
             
-            db_utenti().commit()
-            return redirect(url_for('#'))
+            data.commit()
+            data.close()
+
+            return redirect(url_for('index'))
+
+    return render_template('registrazione.html')
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def accesso():
+    if request=='POST':
+        utente=request.form['user'].encode('UTF-8')
+        passwd=requst.form['pass'].encode('UTF-8')
 
+        database_ram=sqlite3.connect('archivio_utenti.db')
+        cursore=database_ram.cursor()
+
+        cursore.execute('SELECT * FROM utenti WHERE nome=?', (utente,))
+        trovato_nome=cursore.fetchone()
+
+        cursore.execute('SELECT * FROM utenti WHERE passwo=?', (passwd,))
+        trovato_ps=cursore.fetchone()
+
+        database_ram.close()
+
+    if ((trovato_nome is not None and bcrypt.checkpw(utente, trovato_nome[1])) and ( trovato_ps is not None and bcrypt.chekpw (passwd, trovato_ps[7] ) ) ):
+          sessione['id_utenti']=trovato_nome[0]
+          return redirect(url_for('registrazione'))
+    else:
+          return redirect(url_for('index'))
 """
     MODIFICARE IL CODICE SOTTOSTANTE PRIMA DI ESEGUIRE IL DEBUG IN PARTICOLARE IL NUMERO DELLA PORTA.
     DOVREBBE ESSERE app.run(host='0.0.0.0', port=xxxx)
